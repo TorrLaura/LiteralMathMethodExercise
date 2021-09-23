f7193f339e0ffd9749c40a3299aa89f69ddce93d
“TorrLaura” <laplebe35@gmail.com>
2021-09-23 09:56:38 -0500

    reviewed LiteralMathMethodExercise and corrected number 3a and 4

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let warmHugs = `Hi I'm Olaf, and I like warm hugs.`;
 // console.log(warmHugs.toUpperCase());
 console.log(warmHugs.replace("like", "love"));
+warmHugs = warmHugs.replace("like", "love");
 let beenImpaled = "Oh, look at that. I've been impaled"
 // console.log(beenImpaled);
 console.log(beenImpaled.slice(17, 36));
+
+let dotDotDot = '..';
+let skullBones = `I don't have a skull${dotDotDot}or bones`
+console.log(skullBones);
+console.log(Math.PI);
+
+let randomNum = Math.random()
+randomNum *= 3;
+randomNum = Math.floor(randomNum);
+randomNum ++;
+console.log(randomNum);
 
