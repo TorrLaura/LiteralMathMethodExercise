90a30175a0d753f466f3d3ebff44c7ef7f2b0086
“TorrLaura” <laplebe35@gmail.com>
2021-09-22 21:52:25 -0500

    completed variablesExercise

---
+++ script.js
@@ -0,0 +1,7 @@
+let warmHugs = `Hi I'm Olaf, and I like warm hugs.`;
+// console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+let beenImpaled = "Oh, look at that. I've been impaled"
+// console.log(beenImpaled);
+console.log(beenImpaled.slice(17, 36));
+
